f20748fa6c8484e7e9fcdd06daf23b18da0c3a60
woonooo <wjddk912@gmail.com>
2024-09-01 02:01:52 +0900

    show_template(interval_start, end) dag 업로드

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,17 @@
+from airflow.models.dag import DAG
+import pendulum
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2024, 8, 22, tz="Asia/Seoul"),
+    catchup=True, 
+) as dag:
+
+    @task(task_id = 'python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+    
+    show_templates()
