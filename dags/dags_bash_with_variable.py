4e4692241ac0a6f03ba77d3fce1aa6e1ba49f5a9
woonooo <wjddk912@gmail.com>
2024-09-01 23:46:37 +0900

    Variable dag 업로드

---
+++ dags/dags_bash_with_variable.py
@@ -0,0 +1,24 @@
+from airflow.models.dag import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+
+with DAG(
+    dag_id='dags_bash_with_variable',
+    schedule='10 9 * * *',
+    start_date=pendulum.datetime(2024, 8, 22, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    var_value = Variable.get("sample_key")
+
+    bash_var_1 = BashOperator(
+        task_id='bash_var_1',
+        bash_command=f'echo variable:{var_value}'
+    )
+
+    # 스케줄의 과부하를 막기 위해 권고하는 코드
+    bash_var_2 = BashOperator(
+        task_id='bash_var_2',
+        bash_command="echo variable:{{var.value.sample_key}}"
+    )
