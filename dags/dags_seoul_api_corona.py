da4e624904fcb1ef49a19cee50e5b3496a42b935
woonooo <wjddk912@gmail.com>
2024-09-02 17:52:30 +0900

    예방접종 path 수정

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -17,11 +17,11 @@ with DAG(
    )
     
     '''서울시 코로나19 백신 예방접종 현황'''
-    tb_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
-        task_id='tb_corona19_vaccine_stat_new',
-        dataset_nm='TbCorona19VaccineStatNew',
-        path='/opt/airflow/files/TbCorona19VaccineStatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
-        file_name='TbCorona19VaccineStatNew.csv'
+    tv_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
+        task_id='tv_corona19_vaccine_stat_new',
+        dataset_nm='tvCorona19VaccinestatNew',
+        path='/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
+        file_name='tvCorona19VaccinestatNew.csv'
    )
     
-    tb_corona19_count_status >> tb_corona19_vaccine_stat_new
+    tb_corona19_count_status >> tv_corona19_vaccine_stat_new
