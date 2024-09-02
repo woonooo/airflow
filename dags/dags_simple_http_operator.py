9a354bc4e129f4083809083c8c489a263ac01678
woonooo <wjddk912@gmail.com>
2024-09-02 13:43:19 +0900

    서울시공공데이터 api로 데이터 가져오는 dag 업로드

---
+++ dags/dags_simple_http_operator.py
@@ -0,0 +1,36 @@
+from airflow.models.dag import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id='dags_simple_http_operator',
+    schedule=None,
+    start_date=pendulum.datetime(2024, 9, 1, tz='Asia/Seoul'),
+    catchup=False,
+) as dag:
+    
+    '''서울시 공공자전거 대여소 정보'''
+    tb_cycle_station_info = SimpleHttpOperator(
+        task_id='tb_cycle_station_info',
+        http_conn_id='openapi.seoul.go.kr',
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCycleStationInfo/1/12/',
+        method='GET',
+        header={
+            'content-type': 'applocation/json',
+            'charset': 'utf-8',
+            'Accept': '*/*'
+        }
+    )
+
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        result = ti.xcom_pull(task_ids='tb_cycle_station_info')
+        import json
+        from pprint import pprint
+
+        pprint(json.load(result))
+
+    tb_cycle_station_info >> python_2()
