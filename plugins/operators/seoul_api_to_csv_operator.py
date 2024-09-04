1e79995db2d5398c598fc77c120aa1bd0edd59eb
woonooo <wjddk912@gmail.com>
2024-09-04 13:16:35 +0900

    url 확인용 print 추가

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -19,7 +19,10 @@ class SeoulApiToCsvOperator(BaseOperator):
         import os
 
         connection = BaseHook.get_connection(self.http_conn_id)
-        self.base_url = f'http://{connection.host}/{connection.port}/{self.endpoint}'
+        self.base_url = f'http://{connection.host}:{connection.port}/{self.endpoint}'
+
+        print(self.endpoint)
+        print(self.base_url)
 
         total_row_df = pd.DataFrame()
         start_row = 1
